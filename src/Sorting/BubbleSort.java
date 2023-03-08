8900cdd8c5b4c70c1eb036f2a3b43dd44c21ff44
ermsch <erman.dogan@hotmail.de>
2023-03-08 13:58:35 +0100

    aufgabe2

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,23 @@
+package Sorting;
+//package net.bitelligence.training.exercise.sorting.bubble_sort;
+
+public class BubbleSort {
+
+    /**
+     * This method sorts an array of integers using the bubble sort algorithm.
+     * The input is an array of integers to be sorted, and the output is the sorted array.
+     * The bubble sort algorithm repeatedly steps through the list to be sorted,
+     * compares each pair of adjacent items and swaps them if they are in the wrong order.
+     * The pass through the list is repeated until no swaps are needed, which indicates that the list is sorted.
+     *
+     * @param numbers The array of integers to be sorted
+     * @return The sorted array of integers
+     */
+
+    public int[] sort(int[] numbers) {
+
+        // ...
+        return numbers;
+    }
+
+}
