4a2407b7b45430baad94c4a4ec0e11fadd12d416
ermsch <erman.dogan@hotmail.de>
2023-03-08 13:27:36 +0100

    aufgabe1

---
+++ src/aufgabe1/Aufgabe1.java
@@ -0,0 +1,27 @@
+package aufgabe1;
+
+public class Aufgabe1 {
+    int a = 5;
+    //5
+    double b = 5.0;
+    //5.0
+    long c = 4L;
+    //4
+    int d = 3 + 9;
+    //9
+    double e = 2 + 6.0;
+    //8.0
+    float f = 5 + 3.0f;
+    //8.0
+    double g = 3d + 1.0f;
+    //4.0
+    long h = 6L * 3;
+    //18
+    float i = 7 / 2;
+    //3.5
+    double j = 9 / 2.0;
+    //4.5
+
+    //k) int = 1
+
+}
