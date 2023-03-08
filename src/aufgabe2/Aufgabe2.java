ba35ff951fa5542b25bff10532f10863ff171ab8
ermsch <erman.dogan@hotmail.de>
2023-03-08 13:59:12 +0100

    aufgabe2

---
+++ src/aufgabe2/Aufgabe2.java
@@ -0,0 +1,66 @@
+package aufgabe2;
+
+public class Aufgabe2 {
+    // i = 0; j = 7; k = 13
+/*
+// Snippet 1
+if (i > 0 || j > 5) {
+        k = 10;
+    }
+
+    //k = 10
+
+// Snippet 2
+if (i > 0 && j > 5) {
+        k = 10;
+    }
+
+    //k = 13
+
+// Snippet 3
+if ((i > 0 && j > 5) || k < 15) {
+        k = 10;
+    }
+
+    //k = 10
+
+// Snippet 4
+if ((i > 0 || j > 5) && k > 15) {
+        k = 10;
+    }
+
+    //k = 13
+
+// Snippet 5
+if (i == 0 & j++ < 5) {
+        k = 10;
+    }
+    //j = 8
+    //k = 13
+
+// Snippet 6
+if (i == 0 && j++ < 5) {
+        k = 10;
+    }
+    //j = 7
+    //k = 13
+
+// Snippet 7
+if (i != 0 && j++ < 5) {
+        k = 10;
+    }
+
+    //k = 13
+
+// Snippet 8
+if (i != 0 & j++ < 5) {
+        k = 10;
+    }
+
+    //j = 8
+    //k = 13
+    */
+
+
+
+}
